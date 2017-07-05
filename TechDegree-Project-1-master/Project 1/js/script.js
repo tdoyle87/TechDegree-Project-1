d4f6528ccd3fcd8ee00f2715035529c30353d30a
Tom Doyle-Engler <tpdoyle87@gmail.com>
2017-07-05 16:26:49 -0400

    updates to quote timout interval

--- TechDegree-Project-1-master/Project 1/js/script.js
+++ TechDegree-Project-1-master/Project 1/js/script.js
@@ -1,6 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-    document.getElementById('loadQuote').addEventListener("click", printQuote, true);
+    document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //quotes array
 var quotes = [
@@ -118,4 +118,5 @@ function printQuote(){
     document.getElementById('quote-box').innerHTML = output;
     //makes random color background each time printQuote is called
     document.body.style.background = getRandomColor();
+    newRandomQuote();
 }
